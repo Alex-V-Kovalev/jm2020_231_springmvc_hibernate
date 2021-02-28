7ee44f3edaa0e063ea2a86ecd3f52acb6b983e30
Alex-V-Kovalev <a.kovalev@mail.ru>
2021-02-28 21:08:40 +0300

    UserController реализованы методы для редактирования, создания, и удаления записей

--- src/main/java/application/springmvc_hibernate/controller/UserController.java
+++ src/main/java/application/springmvc_hibernate/controller/UserController.java
@@ -1,17 +1,71 @@
 package application.springmvc_hibernate.controller;
 
+import application.springmvc_hibernate.model.User;
+import application.springmvc_hibernate.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class UserController {
-    @RequestMapping(value = "/")
-    public String getUsersPage() {
-        return "users-view";
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 
-    @RequestMapping(value = "/edit")
-    public String getEditUserPage() {
-        return "user-edit-view";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView getUsersPage() {
+        List<User> users = userService.getAllUsers();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("users-view");
+        modelAndView.addObject("usersList", users);
+        return modelAndView;
     }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public ModelAndView getEditUserPage(@PathVariable("id") int id) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("user-edit-view");
+        modelAndView.addObject("user", userService.getUserById(id));
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    public ModelAndView editUser(@ModelAttribute("user") User user) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/");
+        userService.update(user);
+        return modelAndView;
+    }
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public ModelAndView getAddUserPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("user-edit-view");
+        return modelAndView;
+    }
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public ModelAndView addUser(@ModelAttribute("user") User user) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/");
+        userService.create(user);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public ModelAndView deleteUser(@PathVariable("id") int id) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/");
+        userService.delete(userService.getUserById(id));
+        return modelAndView;
+    }
+
+
 }
